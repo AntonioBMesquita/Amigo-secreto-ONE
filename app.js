6ee83053a2793875b23e56f0082044ff8fed7294
Antônio Mesquita <netoa494@gmail.com>
2025-08-07 15:04:27 -0300

    novas funções

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+  const novoAmigo = document.getElementById("amigo").value.trim();
+  const nomeAmigos = document.getElementById("listaAmigos");
+  if (novoAmigo === "") {
+    alert("Insira um nome válido!");
+    return;
+  }
+  let itemLista = document.createElement("li");
+  itemLista.textContent = novoAmigo;
+  nomeAmigos.appendChild(itemLista);
+  amigos.push(novoAmigo);
+  document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("A lista de amigos está vazia. Adicione nomes para sortear.");
+    return;
+  }
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceAleatorio];
+  let resultadoElemento = document.getElementById("resultado");
+  resultadoElemento.innerHTML = `O amigo sorteado é: ${amigoSorteado}!`;
+}
